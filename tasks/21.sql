4ddc890cadbd2e65b886e6a9f9033db1f62e94b0
YunusovSamat <samat.yunusov@bk.ru>
2019-11-17 13:40:34 +0300

    Added task 21

---
+++ tasks/21.sql
@@ -0,0 +1,7 @@
+SELECT maker, MAX(price)
+  FROM product JOIN pc
+    ON product.model = pc.model
+  GROUP BY maker
+-- Задание: 21 (Serge I: 2003-02-13)
+-- Найдите максимальную цену ПК, выпускаемых каждым производителем, у которого есть модели в таблице PC.
+-- Вывести: maker, максимальная цена.
