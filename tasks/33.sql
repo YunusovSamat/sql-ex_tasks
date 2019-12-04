09dee127e79bb9f6983e4e39c257547efa783b27
YunusovSamat <samat.yunusov@bk.ru>
2019-12-04 19:15:44 +0300

    Added task 33

---
+++ tasks/33.sql
@@ -0,0 +1,5 @@
+SELECT ship FROM outcomes
+  WHERE battle = 'North Atlantic'
+    AND result = 'sunk'
+-- Задание: 33 (Serge I: 2002-11-02)
+-- Укажите корабли, потопленные в сражениях в Северной Атлантике (North Atlantic). Вывод: ship.
